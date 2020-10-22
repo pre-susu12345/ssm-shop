37cea671690937776963fb956aa47a1e3736476b
junge <junge@163.com>
2020-10-22 16:36:44 +0800

    测试成功

---
+++ ch02-ssm-shop01/src/main/java/com/ssm/shop01/Ch02SsmShop01Application.java
@@ -0,0 +1,13 @@
+package com.ssm.shop01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02SsmShop01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch02SsmShop01Application.class, args);
+	}
+
+}
---
+++ ch02-ssm-shop01/src/test/java/com/ssm/shop01/Ch02SsmShop01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssm.shop01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch02SsmShop01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
